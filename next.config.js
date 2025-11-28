4ab7a8126c67eda332ee72d46542b9e425603877
recalde12 <recalde1830@hotmail.com>
2025-11-28 12:14:25 +0100

    Programa de Fidelidad config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Aquí puedes añadir configuraciones de Next.js si las necesitas en el futuro
-  // Por ejemplo:
-  // reactStrictMode: true,
-  // images: {
-  //   domains: ['example.com'], // Si usas imágenes externas
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co', // Permite cargar imágenes de cualquier proyecto Supabase
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
